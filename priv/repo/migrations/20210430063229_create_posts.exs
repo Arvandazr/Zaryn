ffebc985376ab64e24e210ce61e218abc8aaf9ca
Arvand <azadvararvand@gmail.com>
2021-06-01 21:41:03 +0430

    Add files via upload

--- priv/repo/migrations/20210430063229_create_posts.exs
+++ priv/repo/migrations/20210430063229_create_posts.exs
@@ -2,13 +2,14 @@ defmodule Zaryn.Repo.Migrations.CreatePosts do
   use Ecto.Migration
 
   def change do
-    create table(:posts) do
-      add :url_id, :string
-      add :description, :text
-      add :photo_url, :string
-      add :total_likes, :integer, default: 0
-      add :total_comments, :integer, default: 0
-      add :user_id, references(:users, on_delete: :delete_all)
+    create table(:posts, primary_key: false) do
+      add(:id, :uuid, primary_key: true)
+      add(:url_id, :string)
+      add(:description, :text)
+      add(:photo_url, :string)
+      add(:total_likes, :integer, default: 0)
+      add(:total_comments, :integer, default: 0)
+      add(:user_id, references(:users, type: :uuid, on_delete: :delete_all))
 
       timestamps()
     end
